5b67f4e5642708a29590853f87616c9a35603f0d
Davidcl-lp <dclworker@gmail.com>
2024-01-21 20:32:19 +0000

    App completed

---
+++ src/main/java/software/ulpgc/kata5/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc.kata5;
+import spark.Spark;
+public class Main {
+    public static void main(String[] args){
+        CommandExecutor.put("factorial", new FactorialCommand());
+        Spark.port(8080);
+        Spark.get("/factorial", (req, res) -> CommandExecutor.with(req, res).execute("factorial"));
+    }
+}
