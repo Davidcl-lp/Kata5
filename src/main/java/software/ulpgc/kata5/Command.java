92a86345c87afbc816b77b62d50489fd34eab112
Davidcl-lp <dclworker@gmail.com>
2024-01-21 20:25:44 +0000

    Interface Command and Class FactorialCommand completed

---
+++ src/main/java/software/ulpgc/kata5/Command.java
@@ -0,0 +1,13 @@
+package software.ulpgc.kata5;
+
+public interface Command {
+    Output execute(Input input);
+
+    interface Input {
+        String get(String key);
+    }
+    interface Output {
+        String result();
+        int response();
+    }
+}
